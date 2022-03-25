7bcf99bf43444c3bfc4b2a22b450102e5dd9b818
anasden26 <anastasia.denisha@student.umn.ac.id>
2022-03-25 10:38:40 +0700

    Update 25/3/2022

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+$(document).ready(function () {
+    var $win = $(window);
+  
+    $win.scroll(function () {
+      if ($win.scrollTop() > 0) {
+        $("#nav .container").addClass("scrolltop");
+      } else {
+        $("#nav .container").removeClass("scrolltop");
+      }
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    autohide = document.querySelector(".navbar");
+  
+    navbar_height = document.querySelector(".navbar").offsetHeight;
+  
+    if (autohide) {
+      var last_scroll_top = 0;
+      window.addEventListener("scroll", function () {
+        let scroll_top = window.scrollY;
+        if (scroll_top < last_scroll_top) {
+          autohide.classList.remove("scrolled-down");
+          autohide.classList.add("scrolled-up");
+        } else if (scroll_top < 600) {
+        } else {
+          autohide.classList.remove("scrolled-up");
+          autohide.classList.add("scrolled-down");
+        }
+        last_scroll_top = scroll_top;
+      });
+      // window.addEventListener
+    }
+    // if
+  });
+  // DOMContentLoaded  end
+  
+
 const header = gsap.timeline({});
 
 header.fromTo('.desc h1', {x: -500, opacity: 0}, {x: 0, opacity: 1, duration: 1, delay: 0.2}, 'first')
@@ -39,10 +77,3 @@ $(window).mousemove(function() {
 });
 
 _delay = setInterval(delayCheck, 1000);
-
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
