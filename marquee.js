47e88428ff965a5fbd47b120080b24f5fc02f7f1
anasden26 <anastasia.denisha@student.umn.ac.id>
2022-04-08 15:08:55 +0700

    update

--- marquee.js
+++ marquee.js
@@ -1,35 +1,41 @@
-const tl3 = gsap.timeline();
-tl3
-//   .fromTo(".transisi4-title", { height: 80 }, { height: 0 })
-//   .fromTo(".transisi4-desc", { height: 40 }, { height: 0 })
-//   .fromTo("#slider1", { x: 1300 }, { x: 0 });
+if(window.matchMedia("(min-width: 992px)").matches) {
 
-        .from("#anim1", {opacity: 0, duration:1.2, y:250, ease: 'Power2.out', "z-index":"2"})
-        .from(".anim2", {opacity: 0, duration: 0.5, y: -5, ease: 'power2.easeIn', })
-        .from(".anim3", {opacity: 0, duration:0.4, y: 30, ease: 'power2.easeIn', })
-
-ScrollTrigger.create({
-  animation: tl3,
-  trigger: "#section3-4",
-  start: "-6% center",
-  end: "90% center",
-  toggleActions: "restart none reverse none",
-  // scrub: 1,
-  // markers: true,
-}); 
-
-
-const tl4 = gsap.timeline();
-tl4
-  .from(".anim5", {opacity:0, duration:1.2, y:250, ease: 'power2.out'})
-  .from(".anim6", {opacity:0, duration:0.8, x:-50, ease: 'power3.out' } )
-
-
-ScrollTrigger.create({
-  animation: tl4,
-  trigger: "#section4",
-  start:"-6% center",
-  end: "90% center",
-  toggleActions: "restart none reverse none",
-
-});
+  const tl3 = gsap.timeline();
+  tl3
+  //   .fromTo(".transisi4-title", { height: 80 }, { height: 0 })
+  //   .fromTo(".transisi4-desc", { height: 40 }, { height: 0 })
+  //   .fromTo("#slider1", { x: 1300 }, { x: 0 });
+  
+          .fromTo("#anim1", {opacity: 0}, {opacity: 1, letterSpacing: 0, duration: 1, ease: 'Power2.out'})
+          .fromTo("#anim1", {y: 350}, {y: 0, duration: 1})
+          .from(".anim2", {opacity: 0, duration: 0.5, y: -5, ease: 'power2.easeIn', })
+          .from(".anim3", {opacity: 0, duration:0.4, y: 30, ease: 'power2.easeIn', })
+          .fromTo('.slider', {autoAlpha: 0}, {autoAlpha: 1});
+  
+  ScrollTrigger.create({
+    animation: tl3,
+    trigger: "#section3-4",
+    start: "0% 10%",
+    end: "90% center",
+    toggleActions: "play none none none",
+    // scrub: 1,
+    // markers: true,
+  }); 
+  
+  
+  const tl4 = gsap.timeline();
+  tl4
+    .fromTo(".anim5", {opacity: 0, letterSpacing: 20}, {opacity: 1, letterSpacing: 0, duration: 1  })
+    .fromTo(".anim5", {y: 350}, {y: 0, duration: 1})
+    .fromTo(".anim6", {opacity:0, x:-50}, {opacity: 1, x: 0, duration: 1, ease: 'Power3.easeOut'} )
+  
+  
+  ScrollTrigger.create({
+    animation: tl4,
+    trigger: "#section4",
+    start:"0% 10%",
+    end: "90% center",
+    toggleActions: "play none none none",
+  
+  });
+  }
