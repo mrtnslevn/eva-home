8ae18001715c8e1673dc565b03db135078de2205
muhadiyaksa <muhadiyaksa@gmail.com>
2022-04-21 16:36:27 +0700

    Update Blog and Choose

--- mainAdi.js
+++ mainAdi.js
@@ -1,47 +1,3 @@
-const imageFormPoint = document.querySelector(".imageforForm");
-const imageForm = document.querySelector(".image-form");
-const cardFormPoint = document.querySelector(".cardforForm");
-const cardForm = document.querySelector(".card-form");
-
-const animateOnScroll = (elementAnimate, animateIn, animateOut, marginView) => {
-  return new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          // console.log("masuk nih");
-          elementAnimate.style.transform = `${animateIn}`;
-        } else {
-          // console.log("yah keluar");
-          elementAnimate.style.transform = `${animateOut}`;
-        }
-      });
-    },
-    {
-      root: document,
-      rootMarginTop: `${marginView}`,
-    }
-  );
-};
-
-const observer3 = animateOnScroll(imageForm, "translateY(0%)", "translateY(100%)", "-200px");
-const observer4 = animateOnScroll(cardForm, "translateX(0%)", "translateX(100%)", "-200px");
-
-if(window.matchMedia("(min-width: 992px)").matches) { 
-  const tlForm = gsap.timeline();
-  tlForm.fromTo('.formSection .image-form', {y: 1000}, {y: 0, duration: 0.1}, 'form')
-  .fromTo('.formSection .card-form', {x: 1000}, {x: 0, duration: 0.1}, 'form');
-
-  ScrollTrigger.create({
-    animation: tlForm,
-    trigger: ".formSection",
-    start: "0% 50%",
-    end: "0% 50%",
-    toggleActions: "play none none none",
-    // scrub: 1,
-    // markers: true,
-  }); 
-}
-
 //Slider
 const sliderItems = document.querySelectorAll(".slider-items");
 const card = document.querySelectorAll(".card");
@@ -83,13 +39,25 @@ buttonSliderPrevious.addEventListener("click", function () {
 });
 
 document.addEventListener("click", function (e) {
+  let widthEl = Math.floor(rectangle.width / 2);
   if (e.target.classList.contains("indicator-button")) {
     let nilai = e.target.getAttribute("indeks");
-    i = +nilai * (width + 45);
     indicatorActive(+nilai, sliderIndicator);
-    sliderItems.forEach((el) => {
-      el.style.transform = `translateX(-${i}px)`;
-    });
+
+    if (window.matchMedia("(min-width: 576px)").matches) {
+      i = +nilai * (width + 45);
+      sliderItems.forEach((el) => {
+        el.style.transform = `translateX(-${i}px)`;
+      });
+    } else {
+      if (+nilai === 0) {
+        sliderCard.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      } else if (+nilai === 1) {
+        sliderCard.scrollTo({ top: 0, left: widthEl * 2 + 30, behavior: "smooth" });
+      } else {
+        sliderCard.scrollTo({ top: 0, left: widthEl * 4 + 60, behavior: "smooth" });
+      }
+    }
   }
 });
 
@@ -101,8 +69,9 @@ function indicatorActive(params, element) {
   element[params].classList.add("btn-warning");
 }
 
+//--------------------------------------------------------------------------
+// JAVASCRIPT FOR BLOG
 function textCutter(element) {
-  console.log(element);
   let cardTeks = [];
   element.forEach((el) => {
     cardTeks.push(el.textContent.substring(0, 200));
@@ -113,3 +82,40 @@ function textCutter(element) {
 }
 const cardText = document.querySelectorAll(".card .card-text");
 textCutter(cardText);
+//--------------------------------------------------------------------------
+
+const sliderCard = document.querySelector(".choose .slider .board-slider");
+const sliderCardItems = document.querySelectorAll(".choose .slider .board-slider .slider-items");
+var rectangle = sliderCardItems[0].getBoundingClientRect();
+var style = sliderCardItems[0].currentStyle || window.getComputedStyle(sliderCardItems[0]);
+let y = 0;
+let x = 0;
+
+var isScrolling;
+
+// Listen for scroll events
+sliderCard.addEventListener(
+  "scroll",
+  function (event) {
+    x = sliderCard.scrollLeft;
+    let widthEl = Math.floor(rectangle.width / 2);
+    window.clearTimeout(isScrolling);
+
+    isScrolling = setTimeout(function () {
+      y = x;
+      if (x == y) {
+        if (x < widthEl + 30 && x > 30) {
+          sliderCard.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          indicatorActive(0, sliderIndicator);
+        } else if (x >= widthEl + 30 && x < widthEl * 3 + 90) {
+          sliderCard.scrollTo({ top: 0, left: widthEl * 2 + 30, behavior: "smooth" });
+          indicatorActive(1, sliderIndicator);
+        } else if (x >= widthEl * 3 + 60) {
+          sliderCard.scrollTo({ top: 0, left: widthEl * 4 + 60, behavior: "smooth" });
+          indicatorActive(2, sliderIndicator);
+        }
+      }
+    }, 200);
+  },
+  false
+);
