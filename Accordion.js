78be9582e91b338b31b06a03b3bc45b453c20537
Martinus Rexyfarrel <77793986+mrtnslevn@users.noreply.github.com>
2022-04-27 09:51:16 +0700

    push accordion

--- Accordion.js
+++ Accordion.js
@@ -1,7 +1,7 @@
-const accordion768 = document.getElementsByClassName('accordion-678');
+const accordion = document.getElementsByClassName('contentBox');
 
-for (i=0; i < accordion768.length; i++){
-    accordion768[i].addEventListener('click', function(){
+for (i = 0; i< accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
         this.classList.toggle('active')
     })
 }
