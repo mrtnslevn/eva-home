d6cfb70d20049250056369d91b65b9e67cfdac7a
muhadiyaksa <muhadiyaksa@gmail.com>
2022-05-20 09:16:32 +0700

    Update Margin Bottom Section 2

--- main1.js
+++ main1.js
@@ -1,5 +1,5 @@
 if (window.matchMedia("(max-width: 992px)").matches) {
-  $('#nav li.dropdown>a').attr('data-bs-toggle','dropdown');
+  $("#nav li.dropdown>a").attr("data-bs-toggle", "dropdown");
 }
 
 $(document).ready(function () {
@@ -39,27 +39,16 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // DOMContentLoaded  end
 
-document.querySelector('#nav .first-button').addEventListener('click', function () {
-
-  document.querySelector('#nav .animated-icon1').classList.toggle('open');
+document.querySelector("#nav .first-button").addEventListener("click", function () {
+  document.querySelector("#nav .animated-icon1").classList.toggle("open");
 });
 
-if (
-  screen.width <= 575 &&
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  ) === true
-) {
+if (screen.width <= 575 && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) === true) {
   window.location.reload();
   document.location = "/m";
 }
 $(window).resize(function () {
-  if (
-    screen.width <= 575 &&
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    ) === true
-  ) {
+  if (screen.width <= 575 && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) === true) {
     window.location.reload();
     document.location = "/m";
   }
@@ -67,60 +56,30 @@ $(window).resize(function () {
 
 const header = gsap.timeline();
 
-if(window.matchMedia("(min-width: 992px)").matches) {
+if (window.matchMedia("(min-width: 992px)").matches) {
   header
-    .fromTo(
-      ".desc h1",
-      { x: -500, autoAlpha: 0 },
-      { x: 0, autoAlpha: 1, duration: 0.6, delay: 0.2 },
-      "first"
-    )
-    .fromTo(
-      ".desc p",
-      { x: -500, autoAlpha: 0 },
-      { x: 0, autoAlpha: 1, duration: 0.6, delay: 0.2 },
-      "first"
-    )
-    .fromTo(
-      ".employee-img",
-      { display: "none", autoAlpha: 0 },
-      { display: "block", autoAlpha: 1, duration: 0.6 },
-      "second"
-    )
-    .fromTo(
-      ".title",
-      { display: "none", autoAlpha: 0 },
-      { display: "block", autoAlpha: 1, duration: 0.6 },
-      "second"
-    )
-    .fromTo(
-      ".rect",
-      { y: 1000, display: "none" },
-      { y: 0, display: "block", duration: 0.6, delay: 0.1 },
-      "third"
-    )
-    .fromTo(
-      ".desc .btn .solusiButton",
-      { display: "none", autoAlpha: 0 },
-      { display: "block", autoAlpha: 1, duration: 0.6, delay: 0.1 },
-      "third"
-    )
+    .fromTo(".desc h1", { x: -500, autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.6, delay: 0.2 }, "first")
+    .fromTo(".desc p", { x: -500, autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.6, delay: 0.2 }, "first")
+    .fromTo(".employee-img", { display: "none", autoAlpha: 0 }, { display: "block", autoAlpha: 1, duration: 0.6 }, "second")
+    .fromTo(".title", { display: "none", autoAlpha: 0 }, { display: "block", autoAlpha: 1, duration: 0.6 }, "second")
+    .fromTo(".rect", { y: 1000, display: "none" }, { y: 0, display: "block", duration: 0.6, delay: 0.1 }, "third")
+    .fromTo(".desc .btn .solusiButton", { display: "none", autoAlpha: 0 }, { display: "block", autoAlpha: 1, duration: 0.6, delay: 0.1 }, "third")
     .fromTo("nav", { autoAlpha: 0 }, { autoAlpha: 1, duration: 0.6, delay: 0.1 }, "third")
-    .fromTo(".icon-scroll", {autoAlpha: 0}, {autoAlpha: 1, duration: 0.6, delay: 0.1}, "third");
+    .fromTo(".icon-scroll", { autoAlpha: 0 }, { autoAlpha: 1, duration: 0.6, delay: 0.1 }, "third");
 }
 
-$('.scroll-down-icon').click(function() {
-  $('html, body').animate({ scrollTop: $('#bagianEVA').offset().top});
-})
+$(".scroll-down-icon").click(function () {
+  $("html, body").animate({ scrollTop: $("#bagianEVA").offset().top });
+});
 
-$('.section1 .desc .btn').click(function() {
-  $('html, body').animate({ scrollTop: $('.formSection').offset().top})
-})
+$(".section1 .desc .btn").click(function () {
+  $("html, body").animate({ scrollTop: $(".formSection").offset().top });
+});
 
-$('#section3-4 .anim3 .btn').click(function() {
-  $('html, body').animate({ scrollTop: $('.formSection').offset().top})
-})
+$("#section3-4 .anim3 .btn").click(function () {
+  $("html, body").animate({ scrollTop: $(".formSection").offset().top });
+});
 
-$('.back-button button').click(function () {
-  $("html, body").animate({ scrollTop: $('#flip-card').offset().top - 80});
+$(".back-button button").click(function () {
+  $("html, body").animate({ scrollTop: $("#flip-card").offset().top - 80 });
 });
